df061f75d3fc4f24f19e0fae9c03f0b60dd1223f
Madi <57211874+Madiba12-prog@users.noreply.github.com>
2022-12-20 20:41:13 +0300

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+"""Square generation module for Python project 0x06
+"""
 
+
+class Square:
+    """Class defined for square generation.
+    Args:
+        size (int): length of one side of square
+    Attributes:
+        __size (int): length of one side of square
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
-        ar = self.__size * self.__size
-        return ar
+        """Calulates area of square.
+        Attributes:
+            __size (int): length of one side of square
+        Returns:
+            area (int): length of one side, squared
+        """
+        area = self.__size * self.__size
+        return 
