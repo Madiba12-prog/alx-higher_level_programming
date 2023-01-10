e3f519c8995d9da2dc47f197dc19f2f09e4f1e4a
Madi <57211874+Madiba12-prog@users.noreply.github.com>
2023-01-10 19:17:27 +0300

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""read_file module.
-Contains a function that reads a text file.
-"""
-
-
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, 'r') as f:
-        print(f.read(), end='')
+    """function that reads a text file and prints it"""
+
+    with open(filename) as f:
+        text = f.read()
+        print(text, end="")
